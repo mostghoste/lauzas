0bfed0f95ba6450446d578584d81f703c1be8ef7
mostghoste <sarunas.jacikas@gmail.com>
2026-02-21 19:44:33 +0200

    Add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
+
+inject()
 
 function updateAppHeight() {
   const h = window.visualViewport?.height ?? window.innerHeight
